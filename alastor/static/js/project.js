9b063263b9173218c45066e801a1cfdb52b9f272
= <yajosev@gmail.com>
2020-05-22 17:56:51 -0600

    Mas cambios

--- alastor/static/js/project.js
+++ alastor/static/js/project.js
@@ -1,12 +1,15 @@
 /* Project specific Javascript goes here. */
 jQuery(document).ready(function($) {
   var menu = $('#general-nav .dropdown');
+  var logo = $('#logo-small');
   $(document).scroll(function() {
     if ($(this).scrollTop() > 195) {
       $(menu).removeClass('d-none');
+      // $(logo).removeClass('d-none');
     }
     else {
       $(menu).addClass('d-none');
+      // $(logo).addClass('d-none');
     }
   });
   if ($('.version-list').length) {
