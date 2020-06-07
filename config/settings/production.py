5bd52d370b0a8ebffbc76ed217f8851afc4640ab
= <yajosev@gmail.com>
2020-06-07 17:35:23 -0600

    strategy

--- config/settings/production.py
+++ config/settings/production.py
@@ -113,6 +113,7 @@ MEDIA_URL = 'https://s3.amazonaws.com/%s/media/' % AWS_STORAGE_BUCKET_NAME
 
 STATIC_URL = 'https://s3.amazonaws.com/%s/static/' % AWS_STORAGE_BUCKET_NAME
 STATICFILES_STORAGE = 'config.settings.production.StaticRootS3BotoStorage'
+COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 # See: https://github.com/antonagestam/collectfast
 # For Django 1.7+, 'collectfast' should come before
 # 'django.contrib.staticfiles'
